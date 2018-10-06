a69b6e0252d4fdb03d35bb34784ad5fc92d36506
Sagar <sagar.thakur39@gmail.com>
2018-10-06 15:48:59 +0400

    red higlihited part moved below pay term in venor bill

--- gt_order_mgnt/models/product.py
+++ gt_order_mgnt/models/product.py
@@ -30,6 +30,40 @@ from urllib import urlencode
 
 class productProduct(models.Model):
     _inherit = 'product.product'
+    
+    qty_scrapped=fields.Float(compute='action_open_scrap',digits_compute=dp.get_precision('Product Unit of Measure'),string='Quantity Scrapped')
+    
+    
+    @api.multi     
+    def action_open_scrap(self):
+        print "sdfsdsdsdf"
+        for line in self:
+            quant_tree = self.env.ref('stock.view_stock_quant_tree', False)
+            quant_form = self.env.ref('stock.view_stock_quant_form', False)
+            scrap_locations=self.env['stock.location'].search([('scrap_location','=',True)])
+            quants_count=self.env['stock.quant'].search([('product_id','=',line.id),('location_id','in',scrap_locations.ids)])
+            print "quants_countquants_countquants_count",quants_count
+            if quants_count:
+                quant_qty=sum(x.qty for x in quants_count)
+                print "quant_qtyquant_qty",quant_qty
+                line.qty_scrapped=quant_qty
+            else:
+                line.qty_scrapped=0.0
+            
+            if quant_tree:
+                return {
+                    'name':'Scrapped',
+                    'type': 'ir.actions.act_window',
+                    'view_type': 'form',
+                    'view_mode': 'tree,',
+                    'res_model': 'stock.quant',
+                    'views': [(quant_tree.id, 'tree'),(quant_form.id, 'form')],
+                    'view_id': quant_tree.id,
+                    'target': 'current',
+                    'domain':[('product_id','=',line.id),('location_id','in',scrap_locations.ids)],
+                }
+        return True
+
 
     @api.v7
     def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
@@ -372,7 +406,7 @@ class productTemplate(models.Model):
     		rec.progress_rate = percentage
     		rec.progress_value = val
                 
-    qty_scrapped=fields.Float(digits_compute=dp.get_precision('Product Unit of Measure'),string='Quantity Scrapped')
+    qty_scrapped=fields.Float(compute='action_open_scrap',digits_compute=dp.get_precision('Product Unit of Measure'),string='Quantity Scrapped')
     produce_delay_qty = fields.Integer('Manufacturing Quantity', default=1)
     product_hs_code=fields.Char('Hs Code')
     net_weight=fields.Float('Net Weight')
@@ -403,20 +437,19 @@ class productTemplate(models.Model):
         print "sdfsdsdsdf"
         for line in self:
             quant_tree = self.env.ref('stock.view_stock_quant_tree', False)
-            print "machine_treemachine_treemachine_tree",machine_tree
             quant_form = self.env.ref('stock.view_stock_quant_form', False)
-            product_id=self.env['product.product'].search([('product_tmpl_id','=',self.id)])
-            print "product_idproduct_idproduct_id",product_id
+            product_id=self.env['product.product'].search([('product_tmpl_id','=',line.id)])
             scrap_locations=self.env['stock.location'].search([('scrap_location','=',True)])
-            quants_count=self.env['stock.quant'].search([('product_id','=',product_id[0].id)])
+            quants_count=self.env['stock.quant'].search([('product_id','=',product_id[0].id),('location_id','in',scrap_locations.ids)])
+            print "quants_countquants_countquants_count",quants_count
             if quants_count:
                 quant_qty=sum(x.qty for x in quants_count)
                 print "quant_qtyquant_qty",quant_qty
-                self.write({'qty_scrapped':quant_qty})
+                line.qty_scrapped=quant_qty
             else:
-                self.write({'qty_scrapped':0.0})
+                line.qty_scrapped=0.0
             
-            if machine_tree:
+            if quant_tree:
                 return {
                     'name':'Scrapped',
                     'type': 'ir.actions.act_window',
