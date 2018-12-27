e465f582f0f21df240062c7b2780e26c59bc6865
aalmir <=test@gmail.com>
2018-12-27 16:05:45 +0400

    view changeeees

--- api_inventory/models/dispatch_bin_picking_list.py
+++ api_inventory/models/dispatch_bin_picking_list.py
@@ -78,7 +78,8 @@ class stockPicking(models.Model):
 		for res in self.pack_operation_product_ids:
 #                    patch below line 80,81 added by bhargavi to handle internal tfr issue in qty
                         if res.picking_id.picking_type_code=='internal' and res.picking_id.location_id.actual_location and res.picking_id.location_dest_id.usage == 'production':
-                            res.write({'qty_done':res.pick_qty,'product_qty':res.pick_qty})
+#                            res.write({'qty_done':res.pick_qty,'product_qty':res.pick_qty})
+                            res.write({'qty_done':res.pick_qty})
 			body += '<li>Product : {} <b>{} {}</b></li>'.format(res.product_id.name,\
 						res.product_uom_id.name,res.pick_qty)
 			#res.qty_done = res.pick_qty
