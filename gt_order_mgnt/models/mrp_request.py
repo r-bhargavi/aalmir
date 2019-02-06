b613994d28befb9be49c25ada2f87eed309c5d7f
Sagar <sagar.thakur39@gmail.com>
2019-02-06 14:37:51 +0400

    menu group bom

--- gt_order_mgnt/models/mrp_request.py
+++ gt_order_mgnt/models/mrp_request.py
@@ -1301,7 +1301,9 @@ class n_manufacturing_request(models.Model):
         mo_form = self.env.ref('mrp.mrp_production_form_view', False)
         print "Self>dfsfsdfsdfdsf",context
         bom_id=self.env['mrp.bom'].search([('product_id','=',self.n_product_id.id)])
-        if bom_id and bom_id.state!='approve':
+        if any(bom.state == 'approve' for bom in bom_id):
+            print "nothing to be done---------------------"
+        else:
             raise UserError('You are not allowed to create MO request as the BoM is still not approved')
 
         if not bom_id:
